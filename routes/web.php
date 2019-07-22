0d662d92febae8d3165cba6b8faecdcdd2bab50c
muttoharoh <muttoharoh10@gmail.com>
2019-07-22 22:07:27 +0700

    Front End

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,51 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/sd1', function () {
+    return view('sd1');
+});
+Route::get('/sd2', function () {
+    return view('sd2');
+});
+Route::get('/sd3', function () {
+    return view('sd3');
+});
+Route::get('/sd4', function () {
+    return view('sd4');
+});
+Route::get('/sd5', function () {
+    return view('sd5');
+});
+Route::get('/sd6', function () {
+    return view('sd6');
+});
+Route::get('/smp1', function () {
+    return view('smp1');
+});
+Route::get('/smp2', function () {
+    return view('smp2');
+});
+Route::get('/smp3', function () {
+    return view('smp3');
+});
+Route::get('/sma1', function () {
+    return view('sma1');
+});
+Route::get('/sma2', function () {
+    return view('sma2');
+});
+Route::get('/sma3', function () {
+    return view('sma3');
+});
+Route::get('/soshum', function () {
+    return view('soshum');
+});
+Route::get('/saintek', function () {
+    return view('saintek');
+});

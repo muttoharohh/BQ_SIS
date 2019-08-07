339ecb14619098b5f0812f35125df1141be093ea
ronaldoblackviper <ronaldoblackviper@gmail.com>
2019-08-07 23:24:35 +0700

    Update SmasatusController.php

--- app/Http/Controllers/SmasatusController.php
+++ app/Http/Controllers/SmasatusController.php
@@ -18,35 +18,35 @@ class SmasatusController extends Controller
 
     
     public function Smasatu_viewadmin(){
-        $Smasatus = \App\Smasatu::all();
+        $smasatus = \App\Smasatu::all();
         return view('admin.sma.sma1crud', compact('Smasatus'));
     }
 
     public function delete($id){
-        $Smasatus = \App\Smasatu::find($id);
-        $Smasatus->delete($id);
+        $smasatus->delete($id);
+        $smasatus = \App\Smasatu::find($id);
         return redirect()->route('admin.sma1');
     }
 
     public function edit($id){
-        $Smasatus = \App\Smasatu::find($id);
+        $smasatus = \App\Smasatu::find($id);
         return view('admin.sma.sma1edit', compact('Smasatus'));
     }
 
     public function update(Request $request, $id){
-        $Smasatus = \App\Smasatu::find($id);
+        $smasatus = \App\Smasatu::find($id);
 
         $cover = $request->file('dokumen');
         if($cover != ""){
             $extension = $cover->getClientOriginalName();
             Storage::disk('sma1dokumen')->put($extension, File::get($cover));
-            $Smasatus->dokumen = 'upload/dokumen/sma1/'.$extension;
+            $smasatus->dokumen = 'upload/dokumen/sma1/'.$extension;
         }
-        $Smasatus->mapel = $request->mapel;
-        $Smasatus->judul = $request->judul;
-        $Smasatus->videopel = 'upload/video/sma1/'.$request->videopel;
-        $Smasatus->dokumen = 'upload/dokumen/sma1/'.$extension;
-        $Smasatus->update();
+        $smasatus->mapel = $request->mapel;
+        $smasatus->judul = $request->judul;
+        $smasatus->videopel = 'upload/video/sma1/'.$request->videopel;
+        $smasatus->dokumen = 'upload/dokumen/sma1/'.$extension;
+        $smasatus->update();
 
         return redirect('/adminsma1');
     }
@@ -62,19 +62,19 @@ class SmasatusController extends Controller
         // $cover = $request->file('videopel');
         // $extension = $cover->getClientOriginalName();
         // Storage::disk('sd1video')->put($extension, File::get($cover));
-        $Smasatus = new \App\Smasatu();
+        $smasatus = new \App\Smasatu();
         $cover = $request->file('dokumen');
         if($cover != ""){
             $extension = $cover->getClientOriginalName();
             Storage::disk('sd2dokumen')->put($extension, File::get($cover));
-            $Smasatus->dokumen = 'upload/dokumen/sma1/'.$extension;
+            $smasatus->dokumen = 'upload/dokumen/sma1/'.$extension;
         }
 
-        $Smasatus->mapel = $request->mapel;
-        $Smasatus->judul = $request->judul;
-        $Smasatus->videopel = 'upload/video/sma1/'.$request->videopel;
+        $smasatus->mapel = $request->mapel;
+        $smasatus->judul = $request->judul;
+        $smasatus->videopel = 'upload/video/sma1/'.$request->videopel;
 
-        $Smasatus->save();
+        $smasatus->save();
 
         return redirect()->route('admin.sma1');
     }
